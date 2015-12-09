59b6de5979e83d5a5f2ad4fab9ce50a0f3f9735d
Alan Parsons <klaxorx@gmail.com>
2015-12-09 23:39:21 +0000

    Some initial Gravity and Thrusting testing

---
+++ Astrodillos/Assets/Scripts/Armadillo.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Astrodillos {
+	public class Armadillo : MonoBehaviour {
+		
+		float jetpackPower = 0.1f;
+		float rotateSpeed = 60f;
+		
+		Rigidbody2D body;
+		// Use this for initialization
+		void Awake () {
+			body = GetComponent<Rigidbody2D>();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKey(KeyCode.Space)){
+				Thrust();
+			}
+
+			UpdateRotation ();
+
+		}
+		
+		void Thrust(){
+			body.AddForce(transform.up * jetpackPower);
+		}
+
+
+		void UpdateRotation(){
+			if (Input.GetKey (KeyCode.D)) {
+				Rotate(-1);
+				return;
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				Rotate(1);
+				return;
+			}
+
+			//Stop rotating
+			Rotate (0);
+		}
+
+		void Rotate(float rotDir){
+			body.angularVelocity = rotDir * rotateSpeed;
+		}
+	}
+}
+
+
