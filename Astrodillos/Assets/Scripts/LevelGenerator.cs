695944072bc70e0713bdcdae4e114addfd702933
shuff264 <jshuff264@gmail.com>
2015-12-15 16:19:05 +0000

    :space_invader: Random planet size

--- Astrodillos/Assets/Scripts/LevelGenerator.cs
+++ Astrodillos/Assets/Scripts/LevelGenerator.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class LevelGenerator : MonoBehaviour {
 
-	public GameObject planet;
+	public GameObject planetPrefab;
+
+	List<GameObject> planetList;
 
 	int numOfPlanets;
 	int xRand;
 	int yRand;
-	int size;
+	int sizeRand;
 
 	// Use this for initialization
 	void Start () {
 	
+		List<GameObject> planetList = new List<GameObject> ();
+
 		numOfPlanets = Random.Range (10, 20);
 		Debug.Log (numOfPlanets);
 
@@ -21,13 +27,20 @@ public class LevelGenerator : MonoBehaviour {
 			xRand = Random.Range(-20, 20);
 			yRand = Random.Range(-10, 10);
 			
-			Instantiate(planet, new Vector2(xRand, yRand), Quaternion.identity);
+			GameObject planet = (GameObject) Instantiate(planetPrefab, new Vector2(xRand, yRand), Quaternion.identity);
+			//planet.transform.localScale = new Vector2(sizeRand, sizeRand);
+			planetList.Add(planet);
 
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+		for (int i = 0; i < planetList.Count; i++) {
+
+			sizeRand = Random.Range (1, 5);
+
+			planetList[i].transform.localScale = new Vector2(sizeRand, sizeRand);
+
+		}
+
+
 	}
 }
