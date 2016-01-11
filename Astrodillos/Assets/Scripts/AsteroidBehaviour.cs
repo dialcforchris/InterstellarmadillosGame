0e67b65debe8c5e581d30ccb8279c726532a6760
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-01-11 16:34:22 +0000

    asteroids
    Tap that Ass(teroid)

---
+++ Astrodillos/Assets/Scripts/AsteroidBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidBehaviour : MonoBehaviour {
+
+    float destroyBuffer = 2.0f;
+    public bool L_or_R;
+    public float speed = 100;
+   
+	// Use this for initialization
+	void Start () 
+    {
+        if (gameObject.transform.position.x>Camera.main.ViewportToWorldPoint(new Vector2(1,0)).x)
+        {
+            speed = speed * -1;
+            L_or_R = true;
+        }
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+      
+        gameObject.transform.Rotate(new Vector3(0, 0, Mathf.PI * Time.deltaTime* -speed));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(Time.deltaTime * speed, 0, 0) ;
+        if (L_or_R)
+        {
+            if (gameObject.transform.position.x+destroyBuffer<Camera.main.ViewportToWorldPoint(new Vector2(0,0)).x)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (!L_or_R)
+        {
+            if (gameObject.transform.position.x-destroyBuffer>Camera.main.ViewportToWorldPoint(new Vector2(1,0)).x)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.transform.GetComponent<AsteroidBehaviour>())
+        {
+            //Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Astrodillos/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AsteroidManager : MonoBehaviour 
+{
+
+    float timer = 0;
+    public GameObject asteroid;
+    int maxAst;
+    float randTime = 0;
+    int currentAsteroids = 0;
+	// Use this for initialization
+	void Start () 
+    {
+        randTime = Random.Range(1.2f, 3.0f);
+        maxAst = Random.Range(5, 10);
+        currentAsteroids = maxAst;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector2 spawnLoc = Vector2.zero; 
+       if (currentAsteroids>0)
+       {
+           float randomY = Random.Range(0.2f, 1.0f);
+           timer += Time.deltaTime;
+           if (randomBool())
+           {
+               spawnLoc.x = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x - 2.0f;
+           }
+           else
+           {
+
+               spawnLoc.x = Camera.main.ViewportToWorldPoint(new Vector2(1,0)).x + 2.0f;
+           }
+           
+           spawnLoc.y = Camera.main.ViewportToWorldPoint(new Vector2(0,randomY)).y;
+           if (timer >= randTime)
+           {
+               Instantiate(asteroid, spawnLoc, new Quaternion(0, 0, 0, 0));
+               
+               timer = 0;
+           }
+
+       }
+       else
+       {
+           currentAsteroids = maxAst;
+       }
+
+	}
+
+    bool randomBool()
+    {
+        return (Random.value > 0.5f);
+    }
+}
